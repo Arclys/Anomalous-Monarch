66571b88ec22e0835492dd2a63d18463fb81ccc0
Arclys <arthur.carvalhocorreacerqueira@gmail.com>
2025-10-10 18:27:59 -0400

    menu v1 e parallax

---
+++ Projeto/Godot/Prototipo/Scripts/title_screen.gd
@@ -0,0 +1,18 @@
+extends Control
+
+func _start_pressed() -> void:
+	get_tree().change_scene_to_file("res://rooms/Room1.tscn")
+func _quit_pressed() -> void:
+	get_tree().quit()
+func _credits_pressed() -> void:
+	pass
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$MarginContainer/HBoxContainer/VBoxContainer/buttonStart.pressed.connect(_start_pressed)
+	$MarginContainer/HBoxContainer/VBoxContainer/buttonQuit.pressed.connect(_quit_pressed)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
