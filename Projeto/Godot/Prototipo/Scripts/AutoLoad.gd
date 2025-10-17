f5eceb6b0adbf335b6a0fd1d7987807d7108a00b
Arclys <arthur.carvalhocorreacerqueira@gmail.com>
2025-10-17 18:17:57 -0400

    edição no menu, project manager e state machine do player

---
+++ Projeto/Godot/Prototipo/Scripts/AutoLoad.gd
@@ -0,0 +1,9 @@
+var player_hp: int = 1
+var player_state: int = 0
+var player_on_floor: bool = false
+var hitable: bool = true
+func _ready() -> void:
+	pass
+
+func _process(delta: float) -> void:
+	pass
