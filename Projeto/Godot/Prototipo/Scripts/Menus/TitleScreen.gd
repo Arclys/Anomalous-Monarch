46eea7bb87ef6114727e32d263dd8856f1cb59d7
oDaniel728 <danielmenezes0822@gmail.com>
2025-11-05 19:04:03 -0400

    Preenchimento Tela Inicial

--- Projeto/Godot/Prototipo/Scripts/Menus/TitleScreen.gd
+++ Projeto/Godot/Prototipo/Scripts/Menus/TitleScreen.gd
@@ -1,5 +1,7 @@
 extends Control
 
+@onready var buttons_location = $MarginContainer/HBoxContainer/VBoxContainer
+
 func _start_pressed() -> void:
 	get_tree().change_scene_to_file("res://Rooms/Room01.tscn")
 func _quit_pressed() -> void:
@@ -7,11 +9,29 @@ func _quit_pressed() -> void:
 func _credits_pressed() -> void:
 	pass
 
+var actions_pressed : Dictionary[String, Callable] = {
+	"buttonStart": func():
+		_start_pressed()
+		,
+	"buttonCredits": func():
+		_credits_pressed()
+		,
+	"buttonQuit": func():
+		_quit_pressed()
+		,
+}
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$MarginContainer/HBoxContainer/VBoxContainer/buttonStart.pressed.connect(_start_pressed)
-	$MarginContainer/HBoxContainer/VBoxContainer/buttonQuit.pressed.connect(_quit_pressed)
-
+	for botao in buttons_location.get_children():
+		if (botao is Button):
+			botao.pressed.connect(
+				func():
+					# se o nome do botao(botao.name) for
+					# uma chave do dicionario
+					actions_pressed.get(botao.name, (func(): pass)).call()
+					# (sim tem que o usar o .call() ao invés do ())
+			)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
--- Projeto/Godot/Prototipo/Scripts/Player/Player.gd
+++ Projeto/Godot/Prototipo/Scripts/Player/Player.gd
@@ -6,8 +6,8 @@ class_name Player
 @export var jump_speed: float = 50.
 @export var mass: float = 0.
 
-var direction = Input.get_axis("ui_left", "ui_right")
-var jump = Input.is_action_just_pressed("ui_accept")
+var direction = Input.get_axis("m_left", "m_right")
+var jump = Input.is_action_just_pressed("m_up")
 
-func move(speed: float, acelleration: float, delta: float) -> float:
-	return lerp(speed, velocity * direction, acelleration * delta)
+func move(_speed: float, acelleration: float, delta: float) -> Vector2:
+	return lerp(Vector2(speed, speed), velocity * direction, acelleration * delta)
