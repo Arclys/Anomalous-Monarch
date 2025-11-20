3f3cbf7ff8280c19ec25501e3413e3b39f6ea4fe
Arclys <arthur.carvalhocorreacerqueira@gmail.com>
2025-11-19 21:00:36 -0400

    s

--- Projeto/Godot/Scripts/Player/States/Death.gd
+++ Projeto/Godot/Scripts/Player/States/Death.gd
@@ -3,13 +3,15 @@ extends State
 
 func _begin_update() -> void:
 	player.sprite.play("Death")
+	player.velocity = Vector2.ZERO
 	animation_ended(self._on_animation_death)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _update(delta: float) -> void:
 	pass
 func _physics_update(delta: float) -> void:
-	pass
+	player.apply_gravity(delta)
+	player.move_and_slide()
 func _end_update() -> void:
 	Master.player_hp = Master.player_max_hp
 	
