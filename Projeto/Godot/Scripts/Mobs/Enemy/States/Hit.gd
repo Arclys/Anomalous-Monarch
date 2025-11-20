e20e0cf264a1c05917aa256e308dce969cdb81f3
Arclys <arthur.carvalhocorreacerqueira@gmail.com>
2025-11-20 18:42:45 -0400

    Update Hit.gd

--- Projeto/Godot/Scripts/Mobs/Enemy/States/Hit.gd
+++ Projeto/Godot/Scripts/Mobs/Enemy/States/Hit.gd
@@ -1,6 +1,7 @@
 extends EnemyState
 func _begin_update() -> void:
 	enemy.sprite.play("Hit")
+	enemy.hp -= 1
 	enemy.velocity = Vector2.ZERO
 	enemy.velocity = Vector2(-enemy.knockback_dir * enemy.knockback_force.x, enemy.knockback_force.y)
 
